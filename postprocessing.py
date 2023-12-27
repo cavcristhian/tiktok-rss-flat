701bb42f664e8ec45671df7e288fe0a8641d5c55
cavcristhian <33798336+cavcristhian@users.noreply.github.com>
2023-12-27 11:00:30 -0500

    Update postprocessing.py

--- postprocessing.py
+++ postprocessing.py
@@ -21,7 +21,7 @@ logger.setLevel(logging.DEBUG)
 # ghPagesURL = "https://conoro.github.io/tiktok-rss-flat/"
 
 # Custom Domain
-ghPagesURL = "https://tiktokrss.conoroneill.com/"
+ghPagesURL = "https://github.com/cavcristhian/tiktok-rss-flat/"
 
 maxItems = 5
 
